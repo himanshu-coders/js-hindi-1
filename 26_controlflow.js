6aabdeca65948df8d64612d10dbb590496f80645
Himanshu <avasthihimanshu98@gmail.com>
2024-06-08 13:51:24 +0530

    my today commit

---
+++ 26_controlflow.js
@@ -0,0 +1,77 @@
+// const isuserloggedin=true
+// if (isuserloggedin){
+
+// }
+//<,>,<=,>=,==,!=,===
+// const score=200
+// if(score>100){
+//     var power="fly"
+//     console.log(`user power:${power}`);
+// }
+// console.log(`user power:${power}`);
+// const balance=1000
+// // if(balance>500) console.log("Test");
+// if(balance>500){
+//     console.log("less than");
+// }else if(balance<750)
+//     {
+//         console.log("less than 750");
+//     }
+//     else if(balance<900)
+//         {
+//             console.log("less than 900");
+//         }
+//         else{
+//             console.log("no balnce");
+//         }
+/*switch(key){
+case value:break;
+default:
+    break;}*/
+//    const month=3
+//  switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+//         break;
+//     case 3:
+//         console.log("march");
+        
+//         break;
+//     case 4:
+//         console.log("april");
+        
+//         break;
+ 
+//     default:
+//         console.log("wrong month");
+//         break;
+//  }   
+//truthy or falsy method
+// const useremail="himanshu@gmail.com"
+// if(useremail){
+//     console.log("got user email");
+// }else{
+//     console.log("dont have use email");
+// }
+//falsy values
+//false,0,-0,BigInt 0n, null,"",undefined,NaN
+//Truthy values
+//"0",'false'," ",[],{},function(){}
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("object is empty");
+// }
+//nullish coalescing operator (??):null undefined
+// let val1;
+// //val1=5??10;
+// val1=null??10
+// console.log(val1);
+// +++++++++terniary opeator
+// conditon?ture:false;
+const iceteaprice=100
+iceteaprice>=80?console.log("less than 80"):console.log("more than 80");;
